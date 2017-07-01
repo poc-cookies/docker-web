654117ca3a33b62747fc07d6f340688281adf677
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-07-01 03:48:55 +0300

    Change default cache invalidation timeout (3600 secs) to 60 secs.

--- static-server.js
+++ static-server.js
@@ -3,7 +3,9 @@ const http = require('http');
 
 const port = 8080;
 
-const staticServer = new static.Server('.');
+const staticServer = new static.Server('.', {
+  cache: 60
+});
 const server = http.createServer((req, res) => {
   staticServer.serve(req, res);
 }).listen(port);
